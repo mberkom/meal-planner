e1a0e4f89a6d2d2d164fe892e80ef7214e97cc42
Daniel Berkompas <daniel@managemyproperty.com>
2013-03-04 16:31:47 -0800

    Add readme, minor changes, install mongoose

---
+++ README.md
@@ -0,0 +1,2 @@
+# Meal Planner
+A meal planning app written entirely in Javascript.  [Node.js](http://nodejs.org) on the backend, [AngularJS](http://angularjs.org) on the front end, and [MongoDB](http://mongodb.org) for the database.
--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -3,15 +3,21 @@
  * Contains all the AngularJS controllers for the mealPlanner app.
  * @copyright Daniel Berkompas, 2013
  */
-define("controllers", ["jquery", "angular", "routes"], function($) {
+define("controllers", ["jquery", "lodash", "angular", "routes"], function($, _) {
   var angular = window.angular;
 
   /*
    * NewGroupMealCtrl
    * Handles creating new group meals.
    */
-  angular.module("mealPlanner").controller("NewGroupMealCtrl", function($scope) {
-    $scope.mealItems = [];
+  angular.module("mealPlanner").controller("NewGroupMealCtrl", function($scope, $location) {
+    // Set up data, using localStorage if possible
+    var storedMeal  = angular.fromJson(window.localStorage['newGroupMeal']) || {};
+    $scope.name       = storedMeal.name;
+    $scope.date       = storedMeal.date;
+    $scope.location   = storedMeal.location;
+    $scope.mealItems  = storedMeal.mealItems || [];
+    $scope.emails     = storedMeal.emails || [];
 
     /*
      * Public: Add a meal item to the mealItems array.
@@ -20,8 +26,7 @@ define("controllers", ["jquery", "angular", "routes"], function($) {
       $scope.mealItems.push({
         name:     $scope.newItemName,
         quantity: $scope.newItemQuantity || 0,
-        assigned: $scope.newItemAssigned,
-        isEditing: false
+        assigned: $scope.newItemAssigned
       });
       _clearNewItemData();
       _focus("newItemName");
@@ -33,9 +38,40 @@ define("controllers", ["jquery", "angular", "routes"], function($) {
      * item - The item Object to remove from the array.
      */
     $scope.removeMealItem = function(item) {
-      $scope.mealItems.push.apply($scope.mealItems.splice($scope.mealItems.indexOf(item), $scope.mealItems.length));
+      var newList = _.reject($scope.mealItems, function(i) { return i.name == item.name });
+      $scope.mealItems = newList;
       _focus("newItemName");
+    };
+
+    /*
+     * Public: Add an email to the emails array.
+     */
+    $scope.addEmail = function() {
+      $scope.emails.push($scope.newEmail);
+      $scope.newEmail = null;
+      _focus("newEmail");
+    };
+
+    /*
+     * Public: Remove an email from the emails array.
+     */
+    $scope.removeEmail = function(email) {
+      var newEmails = _.reject($scope.emails, function(e) { return e == email });
+      $scope.emails = newEmails;
     }
+
+    /*
+     * Public: Convert new Group Meal to JSON.
+     */
+    $scope.toJson = function() {
+      return angular.toJson({
+        name:       $scope.name,
+        date:       $scope.date,
+        location:   $scope.location,
+        mealItems:  $scope.mealItems,
+        emails:     $scope.emails
+      });
+    };
 
     /*
      * Public: Save the group meal to the API.
@@ -43,6 +79,24 @@ define("controllers", ["jquery", "angular", "routes"], function($) {
     $scope.save = function() {
       alert("I'm being saved!");
     };
+
+    /*
+     * Public: Clear local storage and go back to path "/".
+     */
+    $scope.cancel = function() {
+      cancel = confirm("Are you sure you want to cancel and go back to the home page?  You have unsaved changes.")
+      if(cancel) {      
+        window.localStorage['newGroupMeal'] = null;
+        $location.path("/");
+      }
+    };
+
+    /*
+     * Internal: Watch toJson() and update localStorage.
+     */
+    $scope.$watch("toJson()", function(result) {
+      window.localStorage['newGroupMeal'] = result;
+    });
 
     /*
      * Private: Clear the new meal item data.
